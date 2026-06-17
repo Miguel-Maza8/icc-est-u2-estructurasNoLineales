4f5db59fc9ebd064ca4ae6dd99582659869158a2
Miguel Maza <maza68206@gmail.com>
2026-06-17 08:10:26 -0500

    Estructuras no lineales

---
+++ src/structures/node/Node.java
@@ -0,0 +1,49 @@
+package structures.node;
+
+public class Node<T> {
+    private T value ;
+    private Node<T> left;
+    private Node<T> rigtht;
+
+    //CREO UN NODO
+    //INSTANCIO EL NODE -> Constructor
+    // tiene que crearse con el valor 
+
+    public Node(T value){
+        this.value= value;
+        this.left = null;
+        this.rigtht= null;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public Node<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node<T> left) {
+        this.left = left;
+    }
+
+    public Node<T> getRigtht() {
+        return rigtht;
+    }
+
+    public void setRigtht(Node<T> rigtht) {
+        this.rigtht = rigtht;
+    }
+
+    @Override
+    public String toString() {
+        return "Node [" + value + "]";
+    }
+    
+    
+    
+}
---
+++ src/structures/trees/IntTree.java
@@ -0,0 +1,18 @@
+package structures.trees;
+
+import structures.node.Node;
+
+//Clase de arbol de solo enteros
+
+public class IntTree {
+    
+    private Node<Integer> root;
+
+    //Constructor
+
+    public IntTree() {
+        this.root = null;
+    }
+
+    
+}
